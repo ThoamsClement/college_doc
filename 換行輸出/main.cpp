5bdbe296ce9fc4b98fbfc6729daae2d8ef0b673c
hippoiam <hippoasie@gmail.com>
2022-04-23 14:15:28 +0800

    第三題（順便清垃圾）

---
+++ 換行輸出/main.cpp
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/main.cc to edit this template
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: youjou
+ *
+ * Created on 29 March 2022, 10:36
+ */
+
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+/*
+ * 
+ */
+int main() {
+    string Hola = "Hello, how are you?";
+    string ans = "I am fine,thank you. And you?";
+    string final = "Over.";
+    cout <<Hola <<endl<< ans<<endl << final;
+    
+}
