89a43120fc5a43dd71894e5c176495802ff91340
hippoiam <hippoasie@gmail.com>
2022-04-23 13:54:40 +0800

    第貳題

---
+++ 三數總和/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int usr_input_a;
+    int usr_input_b;
+    int usr_input_c;
+    cout << "Input three numbers to add: ";
+    cin >> usr_input_a >> usr_input_b >> usr_input_c;
+    cout << "Your answer: "<<usr_input_a*usr_input_b*usr_input_c;
+}
---
+++ 三數總和/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-MacOSX
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-MacOSX
+CND_ARTIFACT_NAME_Debug=cppapplication_k
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-MacOSX/cppapplication_k
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-MacOSX/package
+CND_PACKAGE_NAME_Debug=cppapplicationk.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-MacOSX/package/cppapplicationk.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-MacOSX
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-MacOSX
+CND_ARTIFACT_NAME_Release=cppapplication_k
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-MacOSX/cppapplication_k
+CND_PACKAGE_DIR_Release=dist/Release/GNU-MacOSX/package
+CND_PACKAGE_NAME_Release=cppapplicationk.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-MacOSX/package/cppapplicationk.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
