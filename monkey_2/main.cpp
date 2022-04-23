20ee088fa7696163484eff2c62cc2c007bf4c130
hippoiam <hippoasie@gmail.com>
2022-04-23 16:30:31 +0800

    上去again

---
+++ monkey_2/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/main.cc to edit this template
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: youjou
+ *
+ * Created on 23 April 2022, 16:27
+ */
+
+#include <cstdlib>
+#include<iostream>
+using namespace std;
+
+int main(){
+    int peach = 18;
+    float day_out = 1.5;
+    cout<<"幾天："<<peach/day_out;
+}
+/*
+ * 
+ */
+
