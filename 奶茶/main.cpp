656a0bdf120dfa85b62c993efb4f4f9b6756329a
hippoiam <hippoasie@gmail.com>
2022-04-24 21:54:38 +0800

    ＋乾話

--- 奶茶/main.cpp
+++ 奶茶/main.cpp
@@ -19,3 +19,10 @@ int main(){
     int time = 600;
     cout << "Green Milk Tea: "<<time/green_mktea<<endl<<"Black Tea: "<<time/black_tea<<endl;
 }
+
+/*  cout << "Green Milk Tea: "<<time/green_mktea<<endl<<"Black Tea: "<<time/black_tea<<endl;
+ * 這句話的大意：cout = printf 讓東西跑出來，就跟在清空購物車後，月底看到信用卡帳單上的東西一樣。
+ *  "Green Milk Tea" = 奶綠，嘿！就醬！ time / green 時間除以奶綠 = 他在這個時間內可以做幾杯奶綠 ，後面的也是。 
+ * endl =  Endline 結束這行。 跟遊戲王一樣，結束這回合
+
+ */
