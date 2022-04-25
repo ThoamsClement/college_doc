444af853c183fe56b5a3fbc2b51c97390c6e70d8
hippoiam <hippoasie@gmail.com>
2022-04-25 15:52:58 +0800

    騎山豬囉

---
+++ reverse/main.cpp
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/main.cc to edit this template
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: youjou
+ *
+ * Created on 25 April 2022, 15:41
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
