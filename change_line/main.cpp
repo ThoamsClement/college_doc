b0b84c6ec8420cd4f404bea0640f7cb7e8ec5604
hippoiam <hippoasie@gmail.com>
2022-04-29 17:10:44 +0800

    程式競賽用

---
+++ change_line/main.cpp
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/main.cc to edit this template
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: youjou
+ *
+ * Created on 29 April 2022, 17:05
+ */
+
+#include <cstdlib>
+#include<iostream>
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    cout << "Hello, how are you?" << endl<<"I am fine thank you. And you?"<<endl << "Over" ;
+}
+
